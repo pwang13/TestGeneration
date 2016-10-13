bfd2ef745a873241f62aea75f73de90d8f4c0816
pwang13 <pwang13@ncsu.edu>
2016-10-12 23:58:40 -0400

    file test

--- main.js
+++ main.js
@@ -100,7 +100,10 @@ function generateTestCases()
 		var trueP = _.some(constraints,{ident:'p'});
 		var trueQ = _.some(constraints,{ident:'q'});
 		var trueY = _.some(constraints,{ident:'y'});
-
+		var buf= _.some(constraints,{ident:'buf'});
+		var options = _.some(constraints,{ident:'options'});
+		var region= _.some(constraints,{ident:'phoneNumber'});
+		var formatString=_.some(constraints,{ident:'formatString'});
 		if (trueY) {
 			var args = Object.keys(params).map( function(k) {return params[k]; }).join(",");
 			console.log("has: "+ functionName + "\n" + args);
@@ -141,36 +144,70 @@ function generateTestCases()
 				console.log(boo);
 				for (bo in boos) {
 					for (b in boos) {
-						for (a in boos) {
-							content += generateTestY(boo, bo, b, a, funcName, args);
+						for (var i = 0; i < 3; i++) {
+							content += generateTestY(boo, bo, b, i, funcName, args);
 						}
 					}
 				}
 			}
-			// content += generateTestY(trueP,trueQ,funcName,args);
-			// content += generateTestY(!trueP,trueQ,funcName,args);
-			// content += generateTestY(trueP,!trueQ,funcName,args);
-			// content += generateTestQP(!trueP,!trueQ,funcName,args);
 		}
 		if( pathExists || fileWithContent )
 		{
-			content += generateMockFsTestCases(pathExists,fileWithContent,funcName, args);
+			content += generateMockFsTestCases(pathExists,fileWithContent, buf, funcName, args);
 			// Bonus...generate constraint variations test cases....
-			content += generateMockFsTestCases(!pathExists,fileWithContent,funcName, args);
-			content += generateMockFsTestCases(pathExists,!fileWithContent,funcName, args);
-			content += generateMockFsTestCases(!pathExists,!fileWithContent,funcName, args);
+			content += generateMockFsTestCases(!pathExists,fileWithContent, buf, funcName, args);
+			content += generateMockFsTestCases(pathExists,!fileWithContent,buf, funcName, args);
+			content += generateMockFsTestCases(!pathExists,!fileWithContent,buf, funcName, args);
+			content += generateMockFsTestCases(pathExists,!fileWithContent,!buf, funcName, args);
+			content += generateMockFsTestCases(pathExists, fileWithContent, buf, funcName, args);
+			content += generateMockFsTestCases(pathExists, fileWithContent, !buf, funcName, args);
+		}
+
+		if(options){
+			content+=generateTestO(true,funcName,args);
+			content+=generateTestO(false,funcName,args);
+		}
+		if(region){
+			content+=generateTestR(true,funcName,args);
+			content+=generateTestR(false,funcName,args);
 		}
 		else
 		{
 			// Emit simple test case.
 			content += "subject.{0}({1});\n".format(funcName, args );
 		}
-
 	}
-
-
 	fs.writeFileSync('test.js', content, "utf8");
+}
 
+function generateTestO(options,funcName,args){
+	var testCase="";
+	var newArgs=args.split(',');
+	
+	var number="'"+faker.phone.phoneNumberFormat().toString()+"'";
+	var numberFormat="'"+faker.phone.phoneFormats().toString()+"'";
+	newArgs[0]=number;
+	newArgs[1]=numberFormat;
+	if(!options){
+		newArgs[2]="false";
+	}
+	testCase+="subject.{0}({1});\n".format(funcName, newArgs);
+	return testCase;
+}
+
+function generateTestR(region,funcName,args){
+	var testCase="";
+	var newArgs=args.split(',');
+	newArgs[0]=newArgs[0].substring(1,4);
+	var number=faker.phone.phoneNumberFormat().toString();
+	if(region){
+		number=newArgs[0]+number.substring(3,12);
+	}
+	number="'"+number+"'";
+	newArgs[0]=number;
+	testCase+="subject.{0}({1});\n".format(funcName, newArgs);
+	console.log("phone: " +testCase);
+	return testCase;
 }
 
 function generateTestY (x, y, z, mode, funcName, args) {
@@ -218,6 +255,11 @@ function generateTestY (x, y, z, mode, funcName, args) {
 		newArgs[3] = str;
 	}
 
+	if (mode == 2) {
+		var str = "'lalla'";
+		newArgs[3] = str;
+	}
+
 	testCase+="subject.{0}({1});\n".format(funcName, newArgs);
 	console.log(testCase);
 	return testCase;
@@ -249,18 +291,29 @@ function generateTestQP (trueP,trueQ,funcName,args)
 }
 
 
-function generateMockFsTestCases (pathExists,fileWithContent,funcName,args) 
+function generateMockFsTestCases (pathExists,fileWithContent, buf, funcName,args) 
 {
 	var testCase = "";
 	// Build mock file system based on constraints.
 	var mergedFS = {};
 	if( pathExists )
 	{
-		for (var attrname in mockFileLibrary.pathExists) { mergedFS[attrname] = mockFileLibrary.pathExists[attrname]; }
+		console.log("in path\n");
+		for (var attrname in mockFileLibrary.pathExists) { mergedFS[attrname] = mockFileLibrary.pathExists[attrname]; 
+			console.log(mergedFS[attrname]);}
 	}
-	if( fileWithContent )
+	if( !fileWithContent )
 	{
 		for (var attrname in mockFileLibrary.fileWithContent) { mergedFS[attrname] = mockFileLibrary.fileWithContent[attrname]; }
+	}
+	if( fileWithContent && !buf){
+		for (var attrname in mockFileLibrary.fileWithContent) { mergedFS[attrname] = mockFileLibrary.fileWithContent[attrname]; }
+		mergedFS['pathContent']['file1']="";
+	}
+	if( fileWithContent && buf){
+		for (var attrname in mockFileLibrary.fileWithContent) { mergedFS[attrname] = mockFileLibrary.fileWithContent[attrname]; }
+		mergedFS['path/fileExists']['file1']="hello";
+		mergedFS['pathContent']['file1']="hi";
 	}
 
 	testCase += 
@@ -316,33 +369,35 @@ function constraints(filePath)
 						// console.log(functionConstraints[funcName].constraints);
 					}
 				}
+				if( child.type === 'BinaryExpression' && child.operator == "==")
+				{
+					if( child.left.type == 'Identifier' && child.left.name=="area")
+					{
+						// get expression from original source code:
+						//var expression = buf.substring(child.range[0], child.range[1]);
+						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
+						functionConstraints[funcName].constraints.push(
+							{
+								ident: 'phoneNumber',
+								value: rightHand,
+							}
+						);
+					}
+				}
 
-
-				// if( child.type === 'BinaryExpression' && child.operator == "&&")
-				// {
-				// 	if( child.left.type == 'BinaryExpression' && child.left.operator==">") {
-				// 		if (child.left.left.type == 'Identifier' && params.indexOf( child.left.left.name ) > -1) {
-				// 			var expression = buf.substring(child.left.range[0], child.range[1]);
-
-				// 		}
-
-				// 		if (child.left.right.type == 'Identifier' && params.indexOf( child.left.right.name ) > -1) {
-
-				// 		}
-				// 	}
-				// 	{
-				// 		// get expression from original source code:
-				// 		//var expression = buf.substring(child.range[0], child.range[1]);
-				// 		var rightHand = buf.substring(child.right.range[0], child.right.range[1])
-				// 		functionConstraints[funcName].constraints.push(
-				// 			{
-				// 				ident: 'phoneNumber',
-				// 				value: rightHand,
-				// 			}
-				// 		);
-				// 	}
-				// }
-
+				if( child.type == "LogicalExpression" && child.operator=='||')
+				{
+					if(child.right.type=='UnaryExpression'){
+						if(child.right.argument.type=='MemberExpression'){
+							functionConstraints[funcName].constraints.push(
+								{
+									ident: child.right.argument.object.name,
+									value: '{normalize: true}'
+								}
+							)
+						}
+					}
+				}
 				
 
 				if( child.type == "CallExpression" && 
